3e5e2763791feae2e8659f0d38ed02fda9ac8279
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2025-07-05 15:53:53 +0700

    Initial Commit

---
+++ Core_Flutter_NativeSide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core_Flutter_NativeSide
+//
+//  Created by Natasha Radika on 05/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Core_Flutter_NativeSideTests/Core_Flutter_NativeSideTests.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Flutter_NativeSideTests.swift
+//  Core_Flutter_NativeSideTests
+//
+//  Created by Natasha Radika on 05/07/25.
+//
+
+import Testing
+@testable import Core_Flutter_NativeSide
+
+struct Core_Flutter_NativeSideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
